d8eb3125248caaca596a0ec094607725ce962e92
atshinn1 <atshinn1@gmail.com>
2015-11-03 20:51:04 -0700

    Test
    1)Sever should receive Location object from client alter the state and
    send it back.
    2)Client should ask for coordinates from keyboard then send location
    object. Then receive the location object back with an altered state.
    3)If everything works the client should see coordinates received before
    being prompted for more

--- Battleship2.0/Source/Location.java
+++ Battleship2.0/Source/Location.java
@@ -2,17 +2,26 @@ public class Location
 {
 	private int m_x;
 	private int m_y;
+	private String m_message;
 	
 	Location()
 	{
 		m_x = 1;
 		m_y = 1;
+		m_message="";
 	}
 	
 	Location(int x, int y)
 	{
 		m_x = x;
 		m_y = y;
+		m_message="";
+	}
+	Location(Location copy)
+	{
+		m_x=copy.x();
+		m_y=copy.y();
+		m_message=copy.getMessage();
 	}
 	public void setLocation(int x, int y)
 	{
@@ -26,5 +35,14 @@ public class Location
 	public int y()
 	{
 		return m_y;
+	}
+
+	//im using this to send messages confirming cooridates recieved. At this point mostly for testing 
+	public String getMessage(){
+		return m_message;
+	}
+
+	public void setMessage(String message){
+		m_message=message;
 	}
 }
